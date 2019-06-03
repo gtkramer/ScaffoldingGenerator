3963256ddfa892e7368cee6c137d47b61e3e1a6a
George Kramer <george.kramer@live.com>
2019-06-02 21:36:46 -0700

    Allow ASCII STL files with different cases
    Convert the content of STL files to lower case so that it matches the
    case that the grammar expects.

--- src/amsg.cpp
+++ src/amsg.cpp
@@ -1,20 +1,28 @@
-#include <iostream>
-
 #include "antlr4-runtime.h"
 #include "StlAsciiLexer.h"
 #include "StlAsciiParser.h"
 #include "CLI.hpp"
 
+std::string getFileContents(std::string filePath) {
+	std::ifstream stream;
+	stream.open(filePath);
+	std::ostringstream content;
+	content << stream.rdbuf();
+	stream.close();
+	return content.str();
+}
+
 int main(int argc, char *argv[]) {
-	CLI::App app{"A 3D printing application that creates the scaffolding required to support an object during production"};
 	std::string stlFilePath;
-	app.add_option("--stl-file", stlFilePath, "Input STL file");
+
+	CLI::App app{"A 3D printing application that creates the scaffolding required to support an object during production"};
+	app.add_option("-s,--stl-file", stlFilePath, "Input STL file");
 	CLI11_PARSE(app, argc, argv);
 
-	std::ifstream stream;
-	stream.open(stlFilePath);
+	std::string stlFile = getFileContents(stlFilePath);
+	std::transform(stlFile.begin(), stlFile.end(), stlFile.begin(), ::tolower);
 
-	antlr4::ANTLRInputStream input(stream);
+	antlr4::ANTLRInputStream input(stlFile);
 	StlAsciiLexer lexer(&input);
 	antlr4::CommonTokenStream tokens(&lexer);
 	StlAsciiParser parser(&tokens);
