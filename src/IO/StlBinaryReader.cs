7e1e4d309881528a1710af129ceacc8febaba5a2
George Kramer <george.kramer@live.com>
2019-06-22 19:05:57 -0700

    Write loops with a proper loop invariant
    Once the loop exits, know that the iterating variable equals the number
    of facets, which means every facet has been processed.
    
    Signed-off-by: George Kramer <george.kramer@live.com>

--- src/IO/StlBinaryReader.cs
+++ src/IO/StlBinaryReader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using AdditiveManufacturing.Mathematics;
 using MathNet.Spatial.Euclidean;
@@ -13,7 +14,7 @@ namespace AdditiveManufacturing.IO {
 				for (int currFacet = 0; currFacet != facetCount; currFacet++) {
 					Vector3D normal = new Vector3D(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
 					Point3D[] vertices = new Point3D[3];
-					for (int currVertex = 0; currVertex < vertices.Length; currVertex++) {
+					for (int currVertex = 0; currVertex != vertices.Length; currVertex++) {
 						vertices[currVertex] = new Point3D(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
 					}
 					facets[currFacet] = new Facet(normal, vertices);
@@ -27,11 +28,11 @@ namespace AdditiveManufacturing.IO {
 			FileInfo fileInfo = new FileInfo(filePath);
 			long fileLength = fileInfo.Length;
 			if ((fileLength - 84) % 50 != 0) {
-				throw new IOException("Corrupt file");
+				throw new Exception("Corrupt file");
 			}
 			int facetCount = (int)((fileLength - 84) / 50);
 			if (facetCount <= 0) {
-				throw new IOException("No facets exist");
+				throw new Exception("No facets exist");
 			}
 			return facetCount;
 		}
