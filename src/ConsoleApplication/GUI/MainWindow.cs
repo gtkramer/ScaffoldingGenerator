e1919c49e1d0cc668abeab7f584cece66d9cb9be
George Kramer <george.kramer@live.com>
2020-05-09 18:21:14 -0700

    Add a GUI namespace
    Keep all of the GUI code together in a dedicated namespace
    
    Signed-off-by: George Kramer <george.kramer@live.com>

---
+++ src/ConsoleApplication/GUI/MainWindow.cs
@@ -0,0 +1,22 @@
+using System;
+using Gtk;
+
+namespace AdditiveManufacturing.GUI
+{
+    class MainWindow : Window
+    {
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) : base(builder.GetObject("MainWindow").Handle)
+        {
+            builder.Autoconnect(this);
+
+            DeleteEvent += Window_DeleteEvent;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+        }
+    }
+}
