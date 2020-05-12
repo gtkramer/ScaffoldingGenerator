f44f5b7df0981560b5a4da7b52fb35e4b027d58a
George Kramer <george.kramer@live.com>
2020-05-11 19:28:05 -0700

    Add shader to GUI namespace
    Signed-off-by: George Kramer <george.kramer@live.com>

--- src/ConsoleApplication/GUI/Shader.cs
+++ src/ConsoleApplication/GUI/Shader.cs
@@ -3,61 +3,63 @@ using System.IO;
 using System.Text;
 using OpenToolkit.Graphics.OpenGL4;
 
-public class Shader : IDisposable
-{
-    public int Handle;
-    private bool DisposedValue = false;
-
-    public Shader(string vertexPath, string fragmentPath)
+namespace AdditiveManufacturing.GUI {
+    public class Shader : IDisposable
     {
-        int vertexShader = CompileShader(vertexPath, ShaderType.VertexShader);
-        int fragmentShader = CompileShader(fragmentPath, ShaderType.FragmentShader);
-        int Handle = GL.CreateProgram();
-        GL.AttachShader(Handle, vertexShader);
-        GL.AttachShader(Handle, fragmentShader);
-        GL.LinkProgram(Handle);
-        GL.DetachShader(Handle, vertexShader);
-        GL.DetachShader(Handle, fragmentShader);
-        GL.DeleteShader(vertexShader);
-        GL.DeleteShader(fragmentShader);
-    }
+        public int Handle;
+        private bool DisposedValue = false;
 
-    private int CompileShader(string shaderPath, ShaderType shaderType) {
-        int shader = GL.CreateShader(shaderType);
-        using (StreamReader reader = new StreamReader(shaderPath, Encoding.UTF8))
+        public Shader(string vertexPath, string fragmentPath)
         {
-            GL.ShaderSource(shader, reader.ReadToEnd());
+            int vertexShader = CompileShader(vertexPath, ShaderType.VertexShader);
+            int fragmentShader = CompileShader(fragmentPath, ShaderType.FragmentShader);
+            int Handle = GL.CreateProgram();
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragmentShader);
+            GL.LinkProgram(Handle);
+            GL.DetachShader(Handle, vertexShader);
+            GL.DetachShader(Handle, fragmentShader);
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
         }
-        GL.CompileShader(shader);
-        string infoLog = GL.GetShaderInfoLog(shader);
-        if (infoLog != System.String.Empty) {
-            System.Console.WriteLine(infoLog);
+
+        private int CompileShader(string shaderPath, ShaderType shaderType) {
+            int shader = GL.CreateShader(shaderType);
+            using (StreamReader reader = new StreamReader(shaderPath, Encoding.UTF8))
+            {
+                GL.ShaderSource(shader, reader.ReadToEnd());
+            }
+            GL.CompileShader(shader);
+            string infoLog = GL.GetShaderInfoLog(shader);
+            if (infoLog != System.String.Empty) {
+                System.Console.WriteLine(infoLog);
+            }
+            return shader;
         }
-        return shader;
-    }
 
-    public void Use()
-    {
-        GL.UseProgram(Handle);
-    }
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
 
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!DisposedValue)
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!DisposedValue)
+            {
+                GL.DeleteProgram(Handle);
+                DisposedValue = true;
+            }
+        }
+
+        ~Shader()
         {
             GL.DeleteProgram(Handle);
-            DisposedValue = true;
         }
-    }
 
-    ~Shader()
-    {
-        GL.DeleteProgram(Handle);
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
