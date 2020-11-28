126814b49da422e9adcdb7ba3ad5dbc17c978c32
George Kramer <george.kramer@live.com>
2020-11-27 19:43:31 -0800

    Refactor Line3D Midpoint function
    Make this a convenience wrapper function around Point3D's MidPoint
    implementation.  Avoid custom implementation of something that is
    already implemented.
    
    Signed-off-by: George Kramer <george.kramer@live.com>

--- src/ConsoleApplication/Extensions/Extensions.cs
+++ src/ConsoleApplication/Extensions/Extensions.cs
@@ -3,12 +3,7 @@ using MathNet.Spatial.Euclidean;
 namespace AdditiveManufacturing.Extensions {
 	public static class MyExt {
 		public static Point3D Midpoint(this Line3D line) {
-			Point3D sp = line.StartPoint;
-			Point3D ep = line.EndPoint;
-			double midX = (sp.X + ep.X) / 2;
-			double midY = (sp.Y + ep.Y) / 2;
-			double midZ = (sp.Z + ep.Z) / 2;
-			return new Point3D(midX, midY, midZ);
+			return Point3D.MidPoint(line.StartPoint, line.EndPoint);
 		}
 	}
 }
