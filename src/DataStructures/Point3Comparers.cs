f541ebec10f03f8a6c0b96e2779bb3da9742e9f9
George Kramer <george.kramer@live.com>
2021-07-02 23:56:59 -0700

    Properly implement IComparer<Point3> interface with nullables
    It's a bit of ceremony to conform to this aspect of the interface, but
    dues must be paid.
    
    This addresses the last of the compiler warnings.

--- src/DataStructures/Point3Comparers.cs
+++ src/DataStructures/Point3Comparers.cs
@@ -4,12 +4,31 @@ using ScaffoldingGenerator.Geometry;
 namespace ScaffoldingGenerator.DataStructures
 {
     public abstract class Point3Comparer : IComparer<Point3> {
-        public abstract int Compare(Point3 a, Point3 b);
+        public int Compare(Point3? a, Point3? b) {
+            bool aIsNull = object.ReferenceEquals(a, null);
+            bool bIsNull = object.ReferenceEquals(b, null);
+            if (aIsNull && !bIsNull) {
+                return -1;
+            }
+            else if (!aIsNull && bIsNull) {
+                return 1;
+            }
+            else if (aIsNull && bIsNull) {
+                return 0;
+            }
+            else {
+                #pragma warning disable 8604
+                return CompareHelper(a, b);
+                #pragma warning restore 8604
+            }
+        }
+
+        protected abstract int CompareHelper(Point3 a, Point3 b);
     }
 
     public class Point3XComparer : Point3Comparer
     {
-        public override int Compare(Point3 a, Point3 b)
+        protected override int CompareHelper(Point3 a, Point3 b)
         {
             if (a.X < b.X)
             {
@@ -50,7 +69,8 @@ namespace ScaffoldingGenerator.DataStructures
 
     public class Point3YComparer : Point3Comparer
     {
-        public override int Compare(Point3 a, Point3 b)
+
+        protected override int CompareHelper(Point3 a, Point3 b)
         {
             if (a.Y < b.Y)
             {
@@ -91,7 +111,7 @@ namespace ScaffoldingGenerator.DataStructures
 
     public class Point3ZComparer : Point3Comparer
     {
-        public override int Compare(Point3 a, Point3 b)
+        protected override int CompareHelper(Point3 a, Point3 b)
         {
             if (a.Z < b.Z)
             {
